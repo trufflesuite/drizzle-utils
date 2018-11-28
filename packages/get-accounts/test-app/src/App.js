b782cad4665883b0f53a124f44eeffe9e0f5c880
Adrian Li <li.adrianmc@gmail.com>
2018-11-28 14:17:31 -0500

    fix get-accounts test-app loading issue

--- packages/get-accounts/test-app/src/App.js
+++ packages/get-accounts/test-app/src/App.js
@@ -3,15 +3,15 @@ import getWeb3 from "@drizzle-utils/get-web3";
 import getAccounts from "@drizzle-utils/get-accounts";
 
 class App extends Component {
-  state = { accounts: null, loading: true };
+  state = { accounts: null };
 
   componentDidMount = async () => {
     const web3 = await getWeb3();
     try {
-      const accounts = await getAccounts({ web3 })
+      const accounts = await getAccounts({ web3 });
       this.setState({ accounts });
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   };
 
@@ -30,11 +30,11 @@ class App extends Component {
   }
 
   render() {
-    const { loading } = this.state;
+    const { accounts } = this.state;
     return (
       <div style={{ margin: "2em" }}>
         <h1>@drizzle-utils/get-accounts</h1>
-        {loading ? "Loading..." : this.renderAccounts()}
+        {accounts === null ? "Loading..." : this.renderAccounts()}
       </div>
     );
   }
