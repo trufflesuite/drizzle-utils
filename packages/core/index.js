43e1443551cbf8ca76dc34479c4fffbdd42397b0
Adrian Li <li.adrianmc@gmail.com>
2018-12-11 01:21:41 -0500

    integrate get-contract-instance w/ core package

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,5 +1,6 @@
 const getWeb3 = require("@drizzle-utils/get-web3");
 const getAccounts = require("@drizzle-utils/get-accounts");
+const getContractInstance = require("@drizzle-utils/get-contract-instance");
 const createCurrentAccount$ = require("@drizzle-utils/current-account-stream");
 
 class DrizzleUtils {
@@ -7,11 +8,25 @@ class DrizzleUtils {
     this.web3 = null;
     this.accounts = null;
     this.currentAccount$ = null;
+    this.contractInstances = [];
   }
 
   async getAccounts() {
     this.accounts = await getAccounts({ web3: this.web3 });
     return this.accounts;
+  }
+
+  async getContractInstance(contractArtifact) {
+    const instance = await getContractInstance({
+      web3: this.web3,
+      contractArtifact,
+    });
+    return instance;
+  }
+
+  async addContract(contractArtifact) {
+    const instance = await this.getContractInstance(contractArtifact);
+    return instance;
   }
 
   async init() {
