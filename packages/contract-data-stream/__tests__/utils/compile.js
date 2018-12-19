cadb0dd4ea41cc9a2e2c7a775b1da75746889276
Adrian Li <li.adrianmc@gmail.com>
2018-12-19 05:14:26 -0500

    add ability to compile contracts

--- packages/contract-data-stream/__tests__/utils/compile.js
+++ packages/contract-data-stream/__tests__/utils/compile.js
@@ -6,15 +6,15 @@ const TruffleCompile = require("truffle-compile");
 const truffleCompile = (...args) =>
   new Promise(resolve => TruffleCompile(...args, (_, data) => resolve(data)));
 
-const compile = async pathToFile => {
-  const sourcePath = path.join(__dirname, pathToFile);
+const compile = async filename => {
+  const sourcePath = path.join(__dirname, "../contracts", filename);
 
   const sources = {
     [sourcePath]: fs.readFileSync(sourcePath, { encoding: "utf8" }),
   };
 
   const options = {
-    contracts_directory: path.join(__dirname),
+    contracts_directory: path.join(__dirname, "../contracts"),
     compilers: {
       solc: {
         version: "0.5.1",
@@ -29,17 +29,18 @@ const compile = async pathToFile => {
     },
   };
 
-  // get file names
-  const sourceFilename = path.basename(sourcePath);
-  const artifactFilename =
-    sourceFilename.substr(0, sourceFilename.lastIndexOf(".")) + `.json`;
+  // generate new file path
+  const artifactFilepath =
+    sourcePath.substr(0, sourcePath.lastIndexOf(".")) + `.json`;
 
   // compile to artifact and stringify
   const artifact = await truffleCompile(sources, options);
   const artifactString = JSON.stringify(artifact, null, 4);
 
   // write artifact to file
-  fs.writeFileSync(artifactFilename, artifactString, "utf8");
+  fs.writeFileSync(artifactFilepath, artifactString, "utf8");
+
+  return artifact;
 };
 
 module.exports = compile;
