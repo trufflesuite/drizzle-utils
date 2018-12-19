349c01f179c81e9f67a1fa2ccebe124a746e36ba
Adrian Li <li.adrianmc@gmail.com>
2018-12-19 04:44:20 -0500

    add compile util

---
+++ packages/contract-data-stream/__test__/compile.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-console */
+const path = require("path");
+const fs = require("fs");
+const TruffleCompile = require("truffle-compile");
+
+// promisify truffle-compile
+const truffleCompile = (...args) =>
+  new Promise(resolve => TruffleCompile(...args, (_, data) => resolve(data)));
+
+const compile = async pathToFile => {
+  const sourcePath = path.join(__dirname, pathToFile);
+
+  const sources = {
+    [sourcePath]: fs.readFileSync(sourcePath, { encoding: "utf8" }),
+  };
+
+  const options = {
+    contracts_directory: path.join(__dirname),
+    compilers: {
+      solc: {
+        version: "0.5.1",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 200,
+          },
+          evmVersion: "byzantium",
+        },
+      },
+    },
+  };
+
+  // get file names
+  const sourceFilename = path.basename(sourcePath);
+  const artifactFilename =
+    sourceFilename.substr(0, sourceFilename.lastIndexOf(".")) + `.json`;
+
+  // compile to artifact and stringify
+  const artifact = await truffleCompile(sources, options);
+  const artifactString = JSON.stringify(artifact, null, 4);
+
+  // write artifact to file
+  fs.writeFileSync(artifactFilename, artifactString, "utf8");
+  console.log("Success!");
+};
+
+module.exports = compile;
--- packages/contract-data-stream/__test__/test_script.js
+++
@@ -1,3 +0,0 @@
-/* eslint-disable no-console */
-const truffleCompile = require("truffle-compile");
-console.log(truffleCompile);
