44bf96f1e5fa2300142123be5591643fd6e9120b
Adrian Li <li.adrianmc@gmail.com>
2018-12-27 14:45:00 -0500

    add test to test observable

--- packages/contract-data-stream/__tests__/node_env.js
+++ packages/contract-data-stream/__tests__/node_env.js
@@ -3,23 +3,26 @@
  */
 const Ganache = require("ganache-core");
 const Web3 = require("web3");
-const compile = require("./utils/compile");
+const createNewBlock$ = require("@drizzle-utils/new-block-stream");
+const { take, finalize, tap, toArray } = require("rxjs/operators");
 
+const compile = require("./utils/compile");
 const createContractData$ = require("../index");
 
 jest.setTimeout(20000);
 
 describe("contract-data-stream tests in node environment", () => {
   let provider;
-  let contractInstance;
+  let web3;
   let accounts;
+  let contractInstance;
   beforeAll(async () => {
     // 1. Compile contract artifact
     const { SimpleStorage } = await compile("SimpleStorage.sol");
 
     // 2. Spawn Ganache test blockchain
     provider = Ganache.provider({ seed: "drizzle-utils" });
-    const web3 = new Web3(provider);
+    web3 = new Web3(provider);
     accounts = await web3.eth.getAccounts();
 
     // 3. Create initial contract instance
@@ -51,5 +54,32 @@ describe("contract-data-stream tests in node environment", () => {
 
   test("createContractData$ function exists", () => {
     expect(createContractData$).toBeDefined();
+  });
+
+  test("can track changes to a call method return value", async done => {
+    const { observable } = await createNewBlock$({
+      web3,
+      pollingInterval: 200,
+    });
+
+    const returnVal$ = await createContractData$({
+      newBlock$: observable,
+      methodCall: contractInstance.methods.get(),
+    });
+
+    returnVal$
+      .pipe(
+        take(2),
+        toArray(),
+        tap(vals => expect(vals).toEqual(["0", "5"])),
+        finalize(() => {
+          expect.assertions(1);
+          done();
+        }),
+      )
+      .subscribe();
+
+    await contractInstance.methods.set(0).send({ from: accounts[0] });
+    await contractInstance.methods.set(5).send({ from: accounts[0] });
   });
 });
