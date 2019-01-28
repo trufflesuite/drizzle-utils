3626da744a651dcdc55cf9cb3feac2c8d7b10899
HL <36248745+honestbonsai@users.noreply.github.com>
2019-01-28 17:24:25 -0500

    Make createContractState$ fn

---
+++ packages/contract-state-stream/index.js
@@ -0,0 +1,21 @@
+const { from } = require("rxjs");
+const { switchMap } = require("rxjs/operators");
+const TruffleDecoder = require("truffle-decoder");
+
+const createContractState$ = options =>
+  new Promise(async resolve => {
+    // TODO - check that the user passed newBlock$, artifact, provider
+    const { newBlock$, artifact, provider } = options;
+
+    // Setting up decoder instance
+    // TODO - support contract inheritance (the 2nd arg)
+    const decoder = TruffleDecoder.forContract(artifact, [], provider);
+    decoder.init();
+
+    // for each new block, we decode the state
+    const observable = newBlock$.pipe(switchMap(() => from(decoder.state())));
+
+    resolve(observable);
+  });
+
+module.exports = createContractState$;
