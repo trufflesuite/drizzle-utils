8d73b08bacffb9b516c3007cb0093f604512b86f
HL <36248745+honestbonsai@users.noreply.github.com>
2019-01-30 18:14:48 -0500

    Check for required options fields Add related tests

--- packages/contract-state-stream/index.js
+++ packages/contract-state-stream/index.js
@@ -3,8 +3,13 @@ const { switchMap } = require("rxjs/operators");
 const TruffleDecoder = require("truffle-decoder");
 
 const createContractState$ = options => {
-  // TODO - check that the user passed newBlock$, artifact, provider
   const { newBlock$, artifact, provider } = options;
+  if (!newBlock$)
+    throw new Error("The options object with newBlock$ is required");
+  if (!artifact)
+    throw new Error("The options object with artifact is required");
+  if (!provider)
+    throw new Error("The options object with provider is required");
 
   // Setting up decoder instance
   // TODO - support contract inheritance (the 2nd arg)
