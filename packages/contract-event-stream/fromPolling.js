4b6c33d81bebfe68524a0203891fb0c0cf84e565
Adrian Li <li.adrianmc@gmail.com>
2019-02-07 10:39:49 -0700

    emit array of events as individual objects

--- packages/contract-event-stream/fromPolling.js
+++ packages/contract-event-stream/fromPolling.js
@@ -1,11 +1,10 @@
 const { from } = require("rxjs");
-const { concatMap } = require("rxjs/operators");
+const { concatMap, flatMap } = require("rxjs/operators");
 
 const fromPolling = ({ contract, newBlock$ }) => {
   const observable = newBlock$.pipe(
-    // TODO, this returns an array, should just be an object like the ws one
-    // Default getPastEvents only returns latest block, will always be an array of 1
     concatMap(() => from(contract.getPastEvents("allEvents"))),
+    flatMap(arr => from(arr)),
   );
 
   return observable;
