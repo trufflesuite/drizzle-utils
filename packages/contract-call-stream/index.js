7e60e69074b2e2180518a8e56d2b7182fbd2eb8b
HL <36248745+honestbonsai@users.noreply.github.com>
2019-02-11 15:20:15 -0500

    Add checks for newBlock$ and methodCall option fields

--- packages/contract-call-stream/index.js
+++ packages/contract-call-stream/index.js
@@ -1,9 +1,12 @@
 const { from } = require("rxjs");
 const { distinctUntilChanged, switchMap } = require("rxjs/operators");
 
-const createContractCall$ = options => {
-  // TODO - check that the user passed newBlock$ and a web3 method call
+const createContractCall$ = (options = {}) => {
   const { newBlock$, methodCall } = options;
+  if (!newBlock$)
+    throw new Error("The options object with newBlock$ is required");
+  if (!methodCall)
+    throw new Error("The options object with methodCall is required");
 
   // for each new block, we call the method
   const observable = newBlock$.pipe(
