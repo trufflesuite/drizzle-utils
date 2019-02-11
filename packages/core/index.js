d6f90b52d92695668fdbd419fb709a5b1597709e
HL <36248745+honestbonsai@users.noreply.github.com>
2019-02-11 15:17:35 -0500

    Remove async from those methods that dont need it

--- packages/core/index.js
+++ packages/core/index.js
@@ -21,14 +21,14 @@ const createDrizzleUtils = async ({ web3 }) => {
   const createCurrentAccount$ = async (options = {}) =>
     await _createCurrentAccount$({ web3, ...options });
 
-  const createContractCall$ = async (options = {}) =>
-    await _createContractCall$({ web3, newBlock$, ...options });
+  const createContractCall$ = (options = {}) =>
+    _createContractCall$({ web3, newBlock$, ...options });
 
-  const createContractEvent$ = async (options = {}) =>
-    await _createContractEvent$({ web3, newBlock$, ...options });
+  const createContractEvent$ = (options = {}) =>
+    _createContractEvent$({ web3, newBlock$, ...options });
 
-  const createContractState$ = async (options = {}) =>
-    await _createContractState$({ web3, newBlock$, ...options });
+  const createContractState$ = (options = {}) =>
+    _createContractState$({ web3, newBlock$, ...options });
 
   return {
     getAccounts,
