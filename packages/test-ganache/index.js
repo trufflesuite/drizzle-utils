524fc9225633008559f698b5ae77c327d4f86130
HL <36248745+honestbonsai@users.noreply.github.com>
2019-02-20 15:59:54 -0500

    Add index to test-ganache

---
+++ packages/test-ganache/index.js
@@ -0,0 +1,26 @@
+const Ganache = require("ganache-core");
+const Web3 = require("web3");
+const compile = require("./compile");
+
+const init = async ({ dirname, filename }) => {
+  // 1. Compile contract artifact
+  const { SimpleStorage } = await compile({ dirname, filename });
+
+  // 2. Spawn Ganache test blockchain
+  const provider = Ganache.provider({ seed: "drizzle-utils" });
+  const web3 = new Web3(provider);
+  const accounts = await web3.eth.getAccounts();
+
+  // 3. Create initial contract instance
+  const instance = new web3.eth.Contract(SimpleStorage.abi);
+
+  // 4. Deploy contract and get new deployed Instance
+  // Note: deployed address located at `deployedInstance._address`
+  const deployedInstance = await instance
+    .deploy({ data: SimpleStorage.bytecode })
+    .send({ from: accounts[0], gas: 150000 });
+
+  return { contractInstance: deployedInstance, provider, web3, accounts };
+};
+
+module.exports = init;
