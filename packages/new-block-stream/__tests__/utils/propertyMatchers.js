8b80ef6b6dbe001023353e8e4572f01099368d57
HL <36248745+honestbonsai@users.noreply.github.com>
2019-02-21 11:46:16 -0500

    Fix snaphots

--- packages/new-block-stream/__tests__/utils/propertyMatchers.js
+++ packages/new-block-stream/__tests__/utils/propertyMatchers.js
@@ -4,6 +4,8 @@ const blockMatcher = {
   timestamp: expect.anything(),
   stateRoot: expect.anything(),
   transactions: [{ blockHash: expect.anything() }],
+  gasUsed: expect.any(Number),
+  number: expect.any(Number),
 };
 
 module.exports = { blockMatcher };
