4c0de8ca9e8681a6f2a6a1fe56fbb2f1666e0c83
Adrian Li <li.adrianmc@gmail.com>
2019-06-10 13:03:33 +0900

    add polling for new blocks w/ RxJS

--- packages/new-block-stream/fromPolling.js
+++ packages/new-block-stream/fromPolling.js
@@ -1,29 +1,54 @@
-const { Observable } = require("rxjs");
-const PollingBlockTracker = require("eth-block-tracker");
+const { timer, from, of } = require("rxjs");
+const { map, concatMap, startWith, pairwise } = require("rxjs/operators");
+
+// helper function to create an array from [x..y]
+const range = (min, max) => {
+  const len = max - min + 1;
+  const arr = new Array(len);
+  for (var i = 0; i < len; i++) {
+    arr[i] = min + i;
+  }
+  return arr;
+};
 
 const fromPolling = ({ web3, pollingInterval }) => {
   // patch missing method
   web3.currentProvider.sendAsync = web3.currentProvider.send;
 
-  // instantiate block tracker
-  const provider = web3.currentProvider;
-  const blockTracker = new PollingBlockTracker({ provider, pollingInterval });
+  // create a stream to poll the blockchain at an interval
+  const timer$ = timer(0, pollingInterval);
 
-  const observable = new Observable(observer => {
-    blockTracker
-      .on("latest", async blockNum => {
-        // get full block info with `web3.eth.getBlock`
-        const block = await web3.eth.getBlock(blockNum, true);
-        observer.next(block);
-      })
-      .on("error", err => observer.next(err));
-  });
+  // a stream of block numbers, which might miss blocks
+  const latestBlockNum$ = timer$.pipe(
+    concatMap(() => from(web3.eth.getBlock("latest"))),
+    map(block => block.number),
+  );
+
+  // a stream of block numbers that do not miss any blocks
+  const nonSkippingBlockNum$ = latestBlockNum$.pipe(
+    startWith(null),
+    pairwise(),
+    concatMap(([last, curr]) => {
+      const isFirstEvent = last === null;
+      const blockSkipped = curr - last > 1;
+
+      if (isFirstEvent || !blockSkipped) {
+        return of(curr);
+      }
+
+      // fill out block numbers for missing blocks
+      const blockNumArray = range(last + 1, curr);
+      return from(blockNumArray);
+    }),
+  );
+
+  const observable = nonSkippingBlockNum$.pipe(
+    concatMap(num => web3.eth.getBlock(num, true)),
+  );
 
   return {
     observable,
-    cleanup: () => {
-      blockTracker.removeAllListeners("latest");
-    },
+    cleanup: () => {},
   };
 };
 
