76c38bb2aabdc8cd636c42b9026693e3817785f2
Adrian Li <li.adrianmc@gmail.com>
2019-06-11 14:02:49 +0900

    use observer pattern in new-block-stream fromSubscribe

--- packages/new-block-stream/fromSubscribe.js
+++ packages/new-block-stream/fromSubscribe.js
@@ -1,24 +1,22 @@
-const { Subject } = require("rxjs");
+const { Observable } = require("rxjs");
 
 const fromSubscribe = ({ web3 }) => {
-  const observable = new Subject();
-  const subscription = web3.eth.subscribe(
-    "newBlockHeaders",
-    async (err, blockHeader) => {
-      if (err) return observable.next(err);
+  const observable = new Observable(subscriber => {
+    const subscription = web3.eth.subscribe(
+      "newBlockHeaders",
+      async (err, blockHeader) => {
+        if (err) return subscriber.next(err);
 
-      // get full block info with `web3.eth.getBlock`
-      const block = await web3.eth.getBlock(blockHeader.number, true);
-      observable.next(block);
-    },
-  );
+        // get full block info with `web3.eth.getBlock`
+        const block = await web3.eth.getBlock(blockHeader.number, true);
+        subscriber.next(block);
+      },
+    );
+    return subscription.unsubscribe;
+  });
 
   return {
     observable,
-    subscription,
-    cleanup: () => {
-      subscription.unsubscribe();
-    },
   };
 };
 
