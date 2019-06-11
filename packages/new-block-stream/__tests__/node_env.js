c238ad82864557147cba74f2ac9e7613b0fab979
Adrian Li <li.adrianmc@gmail.com>
2019-06-11 14:02:49 +0900

    use observer pattern in new-block-stream fromSubscribe

--- packages/new-block-stream/__tests__/node_env.js
+++ packages/new-block-stream/__tests__/node_env.js
@@ -42,7 +42,7 @@ describe("new-block-stream tests in node environment", () => {
   });
 
   test("fromPolling can track blocks", async done => {
-    const { observable: newBlock$, cleanup } = createNewBlock$({
+    const { observable: newBlock$ } = createNewBlock$({
       web3,
       pollingInterval: 200,
     });
@@ -58,7 +58,6 @@ describe("new-block-stream tests in node environment", () => {
         }),
         finalize(() => {
           expect.assertions(2);
-          cleanup();
           done();
         }),
       )
@@ -75,7 +74,7 @@ describe("new-block-stream tests in node environment", () => {
     const web3Ws = new Web3(provider);
     web3Ws.currentProvider.constructor = WebsocketProvider;
 
-    const { observable: newBlock$, cleanup } = createNewBlock$({
+    const { observable: newBlock$ } = createNewBlock$({
       web3: web3Ws,
       pollingInterval: 200,
     });
@@ -91,7 +90,6 @@ describe("new-block-stream tests in node environment", () => {
         }),
         finalize(() => {
           expect.assertions(2);
-          cleanup();
           done();
         }),
       )
