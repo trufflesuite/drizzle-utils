940085473d5c535f67e45cb21e458429e25c942e
Adrian Li <li.adrianmc@gmail.com>
2019-07-30 07:54:11 +0900

    fix unhandled promise rejection error

--- packages/new-block-stream/fromSubscribe.js
+++ packages/new-block-stream/fromSubscribe.js
@@ -5,14 +5,20 @@ const fromSubscribe = ({ web3 }) => {
     const subscription = web3.eth.subscribe(
       "newBlockHeaders",
       async (err, blockHeader) => {
-        if (err) return subscriber.next(err);
+        try {
+          if (err) return subscriber.next(err);
 
-        // get full block info with `web3.eth.getBlock`
-        const block = await web3.eth.getBlock(blockHeader.number, true);
-        subscriber.next(block);
+          // get full block info with `web3.eth.getBlock`
+          const block = await web3.eth.getBlock(blockHeader.number, true);
+          subscriber.next(block);
+        } catch (error) {
+          // eslint-disable-next-line no-console
+          console.error(error);
+          throw error;
+        }
       },
     );
-    return subscription.unsubscribe;
+    return () => subscription.unsubscribe();
   });
 
   return observable;
